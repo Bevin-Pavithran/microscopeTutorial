d5270a5f6b0b9f5a2b1c96f3547d5e5fac299c4d
bevin-pavithran <bevinpavithran@gmail.com>
2016-01-29 11:07:33 +0530

    Created method for updating a post

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -10,12 +10,14 @@ Template.postEdit.events({
       _id: this._id
     };
 
-    Posts.update(currentPostId, {$set: postProperties}, function(error){
-      if (error){
-        alert(error.reason);
-      } else {
-        Router.go('postPage', {_id: currentPostId});
-      }
+    Meteor.call('postUpdate', postProperties, function(error, result){
+      if(error)
+        return alert(error.reason);
+      
+      if(result.postExists)
+        alert('This link has already been posted');
+
+      Router.go('postPage',{_id: result._id});
     });
   },
 
